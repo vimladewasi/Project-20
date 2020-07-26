b48b399111f4b898bc360e202721747570f0ec74
Vimla Dewasi <66508837+vimladewasi@users.noreply.github.com>
2020-07-26 15:40:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 
 var car,wall;
 var speed,weight;  
-var deformation; 
 
 function setup() {
   createCanvas(1600,400);
@@ -22,39 +21,27 @@ function draw() {
   background("black"); 
   
   car.velocityX =speed; 
- 
- // console.log(speed);
-  //console.log(weight);
-  
-  if (car.x - wall.x < wall.width/2 + car.width/2
-    && wall.x - car.x < wall.width/2 + car.width/2) {
-  car.velocityX = car.velocityX * (-1);
-  wall.velocityX = wall.velocityX * (-1);
-  }
 
-  if (car.y - wall.y < wall.height/2 + car.height/2
-  && wall.y - car.y < wall.height/2 + car.height/2) {
-  car.velocityY = car.velocityY * (-1);
-  wall.velocityY = wall.velocityY * (-1);
-  }
-
-  deformation = (0.5*weight*speed*speed)/22500;
-  console.log(deformation);
-
-  if(car.isTouching(wall)){
+  if(wall.x-car.x <(car.width+wall.widhth/2))
+  {
+    car.velocityX=0; 
+    var deformation = 0.5*weight*speed*speed/22500;
+    
     if(deformation<100)
     {
       car.shapeColor = color(0,255,0);
     }
-    if(deformation >=100 && deformation <=180)
+    if(deformation >100 && deformation <180)
     {
       car.shapeColor = color(230,230,0);
     }
-    if(deformation>180)
+    if(deformation >180)
     {
       car.shapeColor = color(255,0,0);
     }
- }
-  
+  }
+    
+  console.log(deformation);
+
   drawSprites();
 }
